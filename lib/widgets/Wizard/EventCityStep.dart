17af5afc52c82d0881e475fe249af28108682772
Moriarty33 <jaminski.ua@gmail.com>
2020-12-13 12:25:21 +0200

    add cities and tags lib

--- lib/widgets/Wizard/EventCityStep.dart
+++ lib/widgets/Wizard/EventCityStep.dart
@@ -1,7 +1,9 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
 import 'StepButton.dart';
 import 'StepInfo.dart';
+import '../../data/cities.dart';
 
 class EventCityStep extends StatelessWidget {
   @override
@@ -14,6 +16,15 @@ class EventCityStep extends StatelessWidget {
             StepInfo(
                 title: "Розкажи нам більше про свою Подію :) ",
                 subtitle: "В якому місті буде подія?"),
+            SizedBox(height: 48),
+            DropdownSearch<String>(
+                mode: Mode.MENU,
+                showSelectedItem: true,
+                showSearchBox: true,
+                items: cities,
+                label: "Вибране місто",
+                onChanged: print,
+                selectedItem: cities[0]),
           ],
         ),
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
